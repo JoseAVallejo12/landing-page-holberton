142f5ee708f4c456ac6c05442fc9e649a8caba8e
Jose A Vallejo <jose.vallejo@globant.com>
2022-05-21 16:03:29 -0500

    fix workpress api

--- src/api/client/index.js
+++ src/api/client/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 const httpRequest = axios.create({
-  baseURL: 'http://josevallejo.zya.me/wp-json/wp/v2/',
-  timeout: 3000,
-  withCredentials: false,
+  baseURL: 'https://josevallejo.zya.me/wp-json/wp/v2/',
+  timeout: 5000,
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+
   },
 });
 
--- src/api/hero/heroService.js
+++ src/api/hero/heroService.js
@@ -8,6 +8,21 @@ const getHeroInfo = async () => {
   return response.data;
 };
 
+
+// const getHeroInfo = async () => {
+//   fetch('https://josevallejo.zya.me/wp-json/wp/v2/pages', {
+//     headers: {
+//       'X-WP-Nonce': 'b8f8f8f8f8',
+//     },
+//     credentials: 'same-origin'
+//   })
+//     .then((resp) => resp.json())
+//     .then((response) => {
+//       //My code
+//       console.log(response);
+//     })
+// }
+
 export {
   getHeroInfo,
 }
