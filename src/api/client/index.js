5c89ad19e7a8b0d2de00a0863c35d727a9a1c721
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2022-05-21 20:08:37 +0000

    update repo

--- src/api/client/index.js
+++ src/api/client/index.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 const httpRequest = axios.create({
   baseURL: 'http://josevallejo.zya.me/wp-json/wp/v2/',
-  timeout: 1000,
-  withCredentials: true,
+  timeout: 3000,
+  withCredentials: false,
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-
+    'Content-Type': 'application/json'
   },
 });
+
 export default httpRequest;
--- src/api/hero/heroService.js
+++ src/api/hero/heroService.js
@@ -1,7 +1,10 @@
 import httpRequest from "../client";
 
 const getHeroInfo = async () => {
-  const response = await httpRequest.get("pages");
+  const response = await httpRequest.get("pages", {
+    xsrfCookieName: "XSRF-TOKEN",
+    xsrfHeaderName: "X-XSRF-TOKEN",
+  });
   return response.data;
 };
 
